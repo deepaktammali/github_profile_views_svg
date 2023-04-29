33b3a8ac527a529f9b4907f576411796bd5aef1f
Deepak Tammali <deepak.tammali627@gmail.com>
2023-04-29 11:15:59 +0530

    feat: Add no-cache to images

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get("/user/:username/views", async (c) => {
   const svgWidth = viewDigitsCount * 8 + 110
 
   c.header('Content-Type', 'image/svg+xml')
-  c.header('Cache-Control', 'no-cache must-revalidate')
+  c.header('Cache-Control', 'no-cache')
 
   return c.body(`<svg width="${svgWidth}" height="24" xmlns="http://www.w3.org/2000/svg">
     <style>
